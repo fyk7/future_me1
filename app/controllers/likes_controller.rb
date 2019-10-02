d0c81c23cd065fdfa50c6dbd2b833e48284b6ef1
fyk7 <tfscofield888@gmail.com>
2019-10-02 13:15:42 +0900

    設定変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,7 +9,7 @@ class LikesController < ApplicationController
   def destroy
     @like = Like.find_by(user_id: current_user.id, micropost_id: params[:micropost_id])
     @like.destroy
-    redirect_to("/microposts/#{params[:micropost_id]}")
+    redirect_to("/microposts/#{params[:micropost_id]}")  
   end
   
 end
