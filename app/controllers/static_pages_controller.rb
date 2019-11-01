146263f841449add88e54ad4c9fa77c464df21d5
fyk7 <tfscofield888@gmail.com>
2019-11-01 21:14:43 +0900

    viewの微修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost_form  = current_user.microposts.build
-      @microposts = Micropost.all.includes(:user).order(created_at: :desc).page(params[:page]).per(15)
+      @microposts = Micropost.all.includes(:user).order(created_at: :desc).page(params[:page]).per(10)
       @user = current_user 
     end
   end
