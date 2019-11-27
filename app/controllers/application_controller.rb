070e1009707bd0cb76a3f49942bd7213e677b2d7
fyk7 <tfscofield888@gmail.com>
2019-11-27 18:30:00 +0900

    話題の投稿の条件を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   before_action :set_tags
-  before_action :set_microposts
-  
-  def search
-    @posts = Micropost.search(params[:search])
-  end
-
+  before_action :set_liked_microposts
+ 
   private
 
   def logged_in_user
     unless logged_in?
       store_location
-      flash[:danger] = "Please log in."
+      flash[:danger] = "ログインしてください"
       redirect_to login_url
     end
   end
@@ -24,9 +20,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def set_microposts
+  def set_liked_microposts
     if logged_in?
-      recent_micropost = Micropost#.order(created_at: :desc).limit(100)
+      recent_micropost = Micropost.where(created_at:1.months.ago..Time.now)
       @recent_microposts = recent_micropost.order(likes_count: :desc).limit(3)
     end
   end
